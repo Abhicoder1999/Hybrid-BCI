4df9f3b03d657bc120304d127ab7c4df0e0b4512
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2019-08-26 21:56:06 +0530

    correct filtering

--- scripts/mental_state.py
+++ scripts/mental_state.py
@@ -1,28 +1,79 @@
 import numpy as np
 import pandas as pd
 import scipy.io
-
+from matplotlib import pyplot as plt
+from scipy.fftpack import fft
+from scipy.signal import butter
+from scipy.signal import sosfilt
 
 def segment (data,Fs):
-    l = data.shape[0]
-    t_total = l/(Fs*60.0)
-    ti
-    data = data
-    win_l = 6;#reading time interval for each segment
-    t_seg = np.array([t_total/6.0 -win_l/2.0,t_total/6 +win_l/2.0,
-                  t_total/2.0-win_l/2.0,t_total/2.0+win_l/2.0,
-                  5*t_total/6.0-win_l/2.0,5*t_total/6.0+win_l/2.0])               
-    return t;
+    
+    data["time"] = tmstamp[:,4];
+    tmax = data["time"][308867]
+    tmin = data["time"][0]
+    k = (tmax-tmin)/3
+    t1 = k + tmin
+    t2 = 2*k + tmin
+    wlen = 6;
+    aseg = data[(data["time"]>=tmin+wlen/2.0) & (data["time"]<=t1-wlen/2.0)]
+    useg = data[(data["time"]>t1+wlen/2.0) & (data["time"]<=t2-wlen/2.0)]
+    sseg = data[(data["time"]>t2+wlen/2.0)&(data["time"]<=tmax-wlen/2.0)]
+    
+    return aseg,useg,sseg;
 
+def preprocessing(data,Fs):
+    data = data - data.mean(axis=0)
+    data = data / (np.abs(data).max(axis = 0)-np.abs(data).min(axis = 0))
+    N,nch = data.shape
+    xfil = np.zeros((N,nch))
+    yfil = np.zeros((N,nch))
+    for i in range(nch):
+            tdata = data[:,i]
+            sos = butter(10, 0.35, 'lowpass', output='sos')
+            tdata_ = sosfilt(sos, tdata)
+            fdata_ = np.abs(fft(tdata_))         
+            xfil[:,i] = tdata_
+            yfil[:,i] = fdata_
+    return xfil,yfil
+    
+def fdesign(tdata,Fs):
+    fdata = fft(tdata)
+    T = np.divide(1,Fs)    
+    N = len(tdata)
+    l = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    nfdata =  2.0/N * np.abs(fdata[0:N//2])
+    
+    sos = butter(10, 0.35, 'lowpass', output='sos')
+    fil_data = sosfilt(sos, tdata)
+    fil_nfdata = 2.0/N * np.abs(fft(fil_data)[0:N//2])
+    
+    plt.subplot(2,1,1)
+    plt.plot(tdata,'r')
+    plt.subplot(2,1,2)
+    plt.plot(l,nfdata,'r')
+    
+    plt.subplot(2,1,1)
+    plt.plot(fil_data)
+    plt.subplot(2,1,2)
+    plt.plot(l,fil_nfdata)
+    
+def dualplt(tdata,fdata,Fs):
+    T = np.divide(1,Fs)    
+    N = len(tdata)
+    l = np.linspace(0.0, 1.0/(2.0*T), N//2)
+    nfdata =  2.0/N * np.abs(fdata[0:N//2])
+    plt.subplot(2,1,1)
+    plt.plot(tdata,'r')
+    plt.subplot(2,1,2)
+    plt.plot(l,nfdata,'r')
+    
+
+def spectrum (segments):
+    pass    
+
+
+#Data Extraction
 file = scipy.io.loadmat('../data/EEG_Data/eeg_record1.mat')
-#print(file.keys())
-#mdata = pd.DataFrame.from_dict(file["o"][0,:])
-#marker = pd.DataFrame.from_dict(file["o"]["marker"][0,0])
-#tag = pd.DataFrame.from_dict(file["o"]["tag"][0,0])
-#data = pd.DataFrame.from_dict(file["o"]["data"][0,0])
-#timestamp = data = pd.DataFrame.from_dict(file["o"]["timestamp"][0,0])
-
-
 mdata = file["o"]
 mtype = mdata.dtype
 ndata = {n: mdata[n][0,0] for n in mtype.names}
@@ -32,14 +83,33 @@ data_raw = ndata["data"]
 marker = ndata["marker"]
 tmstamp = ndata["timestamp"]
 trials = ndata["trials"][0,:,:,:]
-data = pd.DataFrame(data_raw)
-t = segment(data,Fs)
 
+
+data = pd.DataFrame(data_raw)
+segments = segment(data,tmstamp)
+aseg = np.array(segments[0].iloc[0:50000 , 3:17])
+useg = np.array(segments[1].iloc[0:50000 , 3:17])
+sseg = np.array(segments[2].iloc[0:50000 , 3:17])
+
+
+#Preprocessing
+
+x1,y1 = preprocessing(aseg,Fs)
+
+dualplt(x1[:,4][1:1000],y1[:,4][1:1000],Fs)
+#ploting(useg[:,4])
+
+#Feature Exrtraction
+
+#Model Representatino
+
+#Model Training
+
+#Results
 '''
 Steps:
+
 -import all the files data in a loop
--divide data to 3segments of 8 mins as 128Fs 
--find eachs frequency response
 -5 division of the component frequency depending upon region
 -add time kurtosis of the signal
 -ML model NN network for analysis
