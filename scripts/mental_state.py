7e950ab31420a305237d74926a6e41864d07dc99
Abhijeet Tripathy <abhijeetofficial1999@gmail.com>
2019-09-01 11:14:10 +0530

    add batch code

--- scripts/mental_state.py
+++ scripts/mental_state.py
@@ -2,37 +2,48 @@ import numpy as np
 import pandas as pd
 import scipy.io
 from matplotlib import pyplot as plt
+from matplotlib import image
 from scipy.fftpack import fft
 from scipy.signal import butter
 from scipy.signal import sosfilt
 from scipy.signal import spectrogram
 from scipy.ndimage import gaussian_filter
-import tensorflow as tf
+from PIL import Image as img
 import os
-import zipfile
+
 
 
 
 def segment (data,Fs):
-    
-    data["time"] = tmstamp[:,4];
-    tmax = data["time"][308867]
+
+    data["time"] = tmstamp[:,4]
+    l = data.shape[0]
+    tmax = data["time"][l-1]
     tmin = data["time"][0]
+    if tmax < tmin:
+        tmax = tmax + 60
+        temp1 = data["time"] >= 0
+        temp2 = data["time"] < tmin        
+        temp = temp1 & temp2
+        temp = temp*60
+        data["time"] += temp
+    
     k = (tmax-tmin)/3
     t1 = k + tmin
     t2 = 2*k + tmin
-    wlen = 6;
+    wlen = k//2;
+
     aseg = data[(data["time"]>=tmin+wlen/2.0) & (data["time"]<=t1-wlen/2.0)]
     useg = data[(data["time"]>t1+wlen/2.0) & (data["time"]<=t2-wlen/2.0)]
     sseg = data[(data["time"]>t2+wlen/2.0)&(data["time"]<=tmax-wlen/2.0)]
-    
+  
     return aseg,useg,sseg;
 
 def preprocessing(data,Fs):
     N,nch = data.shape
     xfil = np.zeros((N,nch))
     yfil = np.zeros((N,nch))
-    fpass = [2*0.5/Fs,2*20.0/Fs]#from 0.5 to 30hz bpf
+    fpass = [2*0.5/Fs,2*30.0/Fs]#from 0.5 to 30hz bpf
     sos = butter(10, fpass, 'bandpass', output='sos')
     for i in range(nch):
             tdata = data[:,i]
@@ -82,18 +93,23 @@ def dualplt(tdata,fdata,Fs):
 def spectro(tdata,Fs):
     f, t, Sxx = spectrogram(tdata, Fs,nfft = 1024,nperseg = 300)
     Sxx = gaussian_filter(Sxx, sigma=2)
+    return Sxx[0:513//3,:] # only till 20hz matters
+
+def sdesign(tdata,Fs):
+    f, t, Sxx = spectrogram(tdata, Fs,nfft = 1024,nperseg = 300)
+    Sxx = gaussian_filter(Sxx, sigma=2)
     #add opening closing for more prominent effect
     plt.pcolormesh(t,f[0:513//3], Sxx[0:513//3,:])
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.show()
-    pass    
+
 
 def ICA():
     pass
 
 #####Data Extraction
-file = scipy.io.loadmat('../data/EEG_Data/eeg_record8.mat')
+file = scipy.io.loadmat('../data/EEG_Data/eeg_record9.mat')
 mdata = file["o"]
 mtype = mdata.dtype
 ndata = {n: mdata[n][0,0] for n in mtype.names}
@@ -104,41 +120,45 @@ marker = ndata["marker"]
 tmstamp = ndata["timestamp"]
 trials = ndata["trials"][0,:,:,:]
 
-print("ok1")
+
 
 data = pd.DataFrame(data_raw)
+
 segments = segment(data,tmstamp)
+
 data = np.array(data)
 aseg = np.array(segments[0].iloc[0:50000 , 3:17])
 useg = np.array(segments[1].iloc[0:50000 , 3:17])
 sseg = np.array(segments[2].iloc[0:50000 , 3:17])
 
-print("ok2")
+
 #####Preprocessing
 aseg = normalize(aseg)
 useg = normalize(useg)
 sseg = normalize(sseg)
 
-print("ok3")
-spectro(sseg[:,8],Fs)
-#fdesign(aseg[:,4],Fs)
-#xa,ya = preprocessing(aseg,Fs)
-#xu,yu = preprocessing(useg,Fs)
-#xs,ys = preprocessing(sseg,Fs)
+xa,ya = preprocessing(aseg,Fs)
+xu,yu = preprocessing(useg,Fs)
+xs,ys = preprocessing(sseg,Fs)
 
+Sa = spectro(xu[:,2],Fs)
+
+
+######Storing
+
+image.imsave('name2.png', Sa)
+img_load = img.open('name2.png')
+
+
+#sdesign(xs[:,2],Fs)
+#fdesign(sseg[:,2],Fs)
 #dualplt(xa[:,4],ya[:,4],Fs)
-#fdesign(xs[:,5][1:3000],Fs)
 
 #####Feature Exrtraction
 
 
 #Model Representatino
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
 
 #Model Training
 
@@ -148,6 +168,7 @@ Steps:
 
 -import all the files data in a loop
 -spectrogram
+- then acording to division put those in different files
 -ML model NN network for analysis
 -display result
 '''
